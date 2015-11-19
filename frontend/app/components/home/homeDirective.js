5cfba3e49bb3842bb2481a810e526b619f1d9926
dorwi <vidor.kanalas@gmail.com>
2015-11-19 11:56:59 +0100

    change results

--- frontend/app/components/home/homeDirective.js
+++ frontend/app/components/home/homeDirective.js
@@ -3,7 +3,7 @@ angular.module('frontendApp')
 		function HomeCtrl(jsonGetter){
 		  	var self = this;
 		  	
-		    jsonGetter('http://shielded-castle-7285.herokuapp.com/home?seasonName=Betonleague%202015/2016&leagueName1=West&leagueName2=East&roundNumber1=1&roundNumber2=1').success(function(data){
+		    jsonGetter('http://shielded-castle-7285.herokuapp.com/home?seasonName=Betonleague%202015/2016&leagueName1=West&leagueName2=East&roundNumber2=1&roundNumber2=2').success(function(data){
 		      self.response = data;
 		    });
 		}
